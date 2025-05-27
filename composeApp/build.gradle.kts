5d0ed698e6aad2368a4caef7159595c26a8ca80f
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-05-27 13:41:28 +0530

    Bump Gradle and gradle wrapper versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     alias(libs.plugins.composeCompiler)
 
     id("app.cash.sqldelight") version "2.0.2"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("plugin.serialization") version "2.1.21"
 }
 
 kotlin {
