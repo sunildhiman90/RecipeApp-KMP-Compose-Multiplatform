08084d4c0f07cc4f50bc8716cfbc14a22d957a80
Sunil Kumar <sunil.dhiman90@gmail.com>
2026-05-18 16:14:52 +0530

    Implement workaround for SQLDelight upsert issue in `FavoriteRecipeDao` and add delete query

--- composeApp/src/commonMain/kotlin/org/sunildhiman90/recipeappcmp/features/common/data/database/daos/FavoriteRecipeDao.kt
+++ composeApp/src/commonMain/kotlin/org/sunildhiman90/recipeappcmp/features/common/data/database/daos/FavoriteRecipeDao.kt
@@ -5,6 +5,7 @@ import app.cash.sqldelight.async.coroutines.awaitAsOneOrNull
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.sunildhiman90.recipeappcmp.RecipeAppCmpAppDb
 import org.sunildhiman90.recipeappcmp.features.common.data.database.DbHelper
 import org.sunildhiman90.recipeappcmp.features.common.data.database.recipeEntityMapper
 import org.sunildhiman90.recipeappcmp.features.common.domain.entities.RecipeItem
@@ -18,10 +19,45 @@ class FavoriteRecipeDao(
     suspend fun addFavorite(recipeId: Long) {
         val currentDateTime = kotlin.time.Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
         dbHelper.withDatabase { database ->
+
+            //TODO, upsertFavorite is not working in sqldelight 2.1.0, due to this issue: https://github.com/sqldelight/sqldelight/issues/5753
+            // SO currently we have issue in upsert statement, for workaround of running, i added SELECT 1 in the end of statement,.
+            // But now its now executing query for upsert
+            /*database.favoriteRecipeQueries.upsertFavorite(
+                recipe_id = recipeId, added_at = currentDateTime.toString()
+            )*/
+
+            //As of now using this workaround
+            if (isFavorite(database, recipeId)) {
+                //Already favorite, remove it
+                deleteFavoriteRecipeById(
+                    database = database,
+                    recipeId = recipeId
+                )
+            } else {
+                //Not favorite, add it
+                addFavorite(database, recipeId, currentDateTime.toString())//database.favoriteRecipeQueries.insertFavorite(
+            }
             database.favoriteRecipeQueries.upsertFavorite(
                recipe_id = recipeId, added_at = currentDateTime.toString()
             )
         }
+    }
+
+    private suspend fun deleteFavoriteRecipeById(database: RecipeAppCmpAppDb, recipeId: Long) {
+        database.favoriteRecipeQueries.deleteFavoriteRecipeById(
+            recipe_id = recipeId
+        )
+    }
+
+    private suspend fun isFavorite(database: RecipeAppCmpAppDb, recipeId: Long): Boolean {
+        return database.favoriteRecipeQueries.selectFavoriteByRecipeId(recipeId).awaitAsOneOrNull() != null
+    }
+
+    private suspend fun addFavorite(database: RecipeAppCmpAppDb, recipeId: Long, currentDateTime: String) {
+        database.favoriteRecipeQueries.insertFavorite(
+            recipe_id = recipeId, added_at = currentDateTime
+        )
     }
 
     suspend fun removeFavorite(recipeId: Long) {
